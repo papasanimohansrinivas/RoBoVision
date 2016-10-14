c5ce498316c2c6742d288040a2aa142318292c48
papasani\mohan/srinivas <papasani.mohansrinivas@gmail.com>
2016-10-15 05:12:37 +0530

    Add files via upload

---
+++ RoboVision2.py
@@ -0,0 +1,21 @@
+class RobonautVisionR2(object):
+
+    def doneTraining():
+        print "this is doneTraining method"
+
+        return 4
+    def testingPair(leftImage,rightImage):
+        t=[]
+        return t
+    def trainingModel(plyData):
+
+        return 3
+    def trainingPair(leftImage,rigthImage,groundTruth):
+
+        return 5
+
+
+
+new = RobonautVisionR2()
+
+print new.doneTraining
